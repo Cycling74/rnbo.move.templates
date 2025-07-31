5236c0d584e97791774c24e472bfc1258f339e9f
Alex Norman <xnor@cycling74.com>
2025-07-31 09:01:43 -0700

    kbmplayground with mapped output

--- codebox/kbmplayground.js
+++ codebox/kbmplayground.js
@@ -1,7 +1,13 @@
 @state parser = new scala();
 
-let v = in1;
-
 function listin2(kbm: list) {
 	parser.updateMap(kbm);
+}
+
+let v = in1;
+
+let mapped = parser.applyKBM(v);
+
+if (mapped[1]) {
+  out1 = mapped[0];
 }
