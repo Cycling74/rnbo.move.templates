b54f0b6e00d61f471ca57b6f2698120484ac65a8
Alex Norman <xnor@cycling74.com>
2025-09-05 13:38:30 -0700

    add mode to zero out key pressure if it isn't already zero

--- codebox/inputdriver.js
+++ codebox/inputdriver.js
@@ -9,19 +9,31 @@
 //3 -> functions (capture, sampling, repeat, mute, delete, duplicate, undo, shift, record)
 //4 -> nav (plus, minus, left, right)
 
+//when we get a note off, should we send a zero key pressure if one hasn't already been sent?
+@param sendzeropressure = 1;
+
 //parse midi input
 @state parser = new midiparse();
+@state activepressure: Int = 0; //we know that this is at least 32-bits
+
 function in2(bytein: number) {
   //forward midi output
   let m = parser.next(bytein);
   //
   if (m[0] == 1) { //key pressure
     let chan = m[3];
-    if (chan == 16) {
+    let num = m[5] - 68; //convert to 0 based
+    if (chan == 16 && num >= 0 && num < 32) {
       //using 5 & 6 because midiparse or midiformat are broken 
-      let num = m[5] - 68; //convert to 0 based
       let val = m[6];
       listout2 = [num, val];
+
+      let mask: Int = 1 << num;
+      if (val == 0) {
+        activepressure = activepressure & ~mask;
+      } else {
+        activepressure |= mask;
+      }
     }
   }
 }
@@ -39,7 +51,17 @@ if (prefix == 0) { //note
   let num = m[1];
   let vel = m[2];
   if (num >= 68 && num <= 99) {
-    listout1 = [OUT_PAD, num - 68, vel];
+    num -= 68;
+
+    //send a zero pressure if none has already been sent
+    if (sendzeropressure && vel == 0) {
+      let mask: Int = 1 << num;
+      if ((mask & activepressure) != 0) {
+        listout2 = [num, 0];
+      }
+    }
+
+    listout1 = [OUT_PAD, num, vel];
   } else if (num >= 16 && num <= 31) {
     listout1 = [OUT_STEP, num - 16, vel];
   }
